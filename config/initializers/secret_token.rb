1572018eab5724e5751361b383487cfb68758e3b
Austin Curtis <austinjohncurtis@gmail.com>
2013-11-29 17:04:46 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmigoRails::Application.config.secret_key_base = '8aa85f89542da66d5ec36dab0ec41cf352e5cbc10c388b10273891c028e2118e82f862d19b666d094df9867123b321c46f6842830f18d981311974a4aba18bb4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AmigoRails::Application.config.session_store :cookie_store, key: '_AmigoRails_session'
