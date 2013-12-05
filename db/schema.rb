c09f36faaa33953c15874cc83fe17283694edd8f
Austin Curtis <austinjohncurtis@gmail.com>
2013-12-05 07:09:15 -0500

    added user/post associations and authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203114624) do
+ActiveRecord::Schema.define(version: 20131204115351) do
 
   create_table "feed_posts", force: true do |t|
     t.string   "postDesc"
@@ -23,7 +23,10 @@ ActiveRecord::Schema.define(version: 20131203114624) do
     t.string   "postDesc"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
